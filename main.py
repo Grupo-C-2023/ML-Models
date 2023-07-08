e86daed00d6643755f232d200bf212cf69a1ca60
Diego Zavala <grupoc.in2023@gmail.com>
2023-07-08 10:14:34 -0500

    refactor: update imports

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from multiapp import MultiApp
-from deploy import home, svc_model, lstm_model, kmeans_model, random_forest_regression_model, decision_tree_model
+from deploy import home, svc_model, kmeans_model, random_forest_regression_model, decision_tree_model
 app = MultiApp()
 
 st.markdown("# Inteligencia de Negocios - Equipo C - Semana 14 ")
@@ -8,7 +8,6 @@ st.markdown("# Inteligencia de Negocios - Equipo C - Semana 14 ")
 # Add all your application here
 app.add_app("Home", home.app)
 app.add_app("SVC Model", svc_model.app)
-app.add_app("LSTM Model", lstm_model.app)
 app.add_app("KMeans Model", kmeans_model.app)
 app.add_app("Random Forest Regression Model", random_forest_regression_model.app)
 app.add_app("Decision Tree Model", decision_tree_model.app)
